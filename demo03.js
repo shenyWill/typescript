699f4e70bf9b12d89817781054061aa040ca3637
shenyuan <775486362@qq.com>
2018-09-04 10:05:40 +0800

    variable

---
+++ demo03.js
@@ -0,0 +1,9 @@
+"use strict";
+var age = 18;
+var stature = 178.5;
+var tag = NaN;
+console.log(age);
+console.log(stature);
+console.log('--------------------------------');
+var yuanwill = 'yuanwill.com';
+console.log(yuanwill);
---
+++ demo03.ts
@@ -0,0 +1,11 @@
+var age:number = 18;
+var stature:number = 178.5;
+var tag:number = NaN;
+console.log(age)
+console.log(stature);
+
+console.log('--------------------------------');
+
+var yuanwill:string = 'yuanwill.com';
+
+console.log(yuanwill);
